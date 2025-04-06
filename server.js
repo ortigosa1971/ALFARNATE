6690bc9b86184204dfa42d6f552102c87990786d
ortigosa1971 <eliasalfarnate@gmail.com>
2025-04-06 10:15:28 +0200

    Servidor corregido con login y session_id

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = new sqlite3.Database('./db/usuarios.db');
@@ -14,6 +15,7 @@ app.use(session({
   cookie: { maxAge: 3600000 } // 1 hora
 }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -29,6 +31,27 @@ app.get('/verificar-sesion', (req, res) => {
     } else {
       res.sendStatus(200);
     }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { usuario, password } = req.body;
+
+  db.get('SELECT * FROM users WHERE username = ? AND password = ?', [usuario, password], (err, row) => {
+    if (err || !row) {
+      return res.redirect('/login.html?error=1');
+    }
+
+    req.session.user = { username: row.username };
+
+    db.run('UPDATE users SET session_id = ? WHERE username = ?', [req.sessionID, row.username], (err) => {
+      if (err) {
+        console.error("Error actualizando session_id:", err);
+        return res.redirect('/login.html?error=1');
+      }
+
+      res.redirect('/inicio.html');
+    });
   });
 });
 
